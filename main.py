995f7d6cb620db6e76675636d0016cd3475b2723
kcemenike <29731161+kcemenike@users.noreply.github.com>
2019-01-22 13:50:13 +0100

    edits

--- main.py
+++ main.py
@@ -113,4 +113,4 @@ def getParameters():
             return jsonify("Error when getting details - {}".format(e))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
